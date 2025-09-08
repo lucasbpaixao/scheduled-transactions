2b75a0d06fc9040b67baaae47ea6f877dfecb552
lucasbpaixao <lucasbpaixao@outlook.com>
2025-09-08 02:22:02 -0300

    Primeiro Commit

---
+++ src/test/java/com/lucasbpaixao/scheduled_transactions/ScheduledTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasbpaixao.scheduled_transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduledTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
